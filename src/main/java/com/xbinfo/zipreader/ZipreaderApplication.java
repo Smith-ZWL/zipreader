c091d0cb0c6bcb2fe4fed58ad9ce2696e701c652
zhuweiliu <2732564500@qq.com>
2024-09-12 17:30:23 +0800

    init

---
+++ src/main/java/com/xbinfo/zipreader/ZipreaderApplication.java
@@ -0,0 +1,13 @@
+package com.xbinfo.zipreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipreaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipreaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xbinfo/zipreader/ZipreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbinfo.zipreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipreaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
